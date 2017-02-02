a4aa937eff6697d8a753f628f2e3c024799bf165
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2017-02-02 13:52:13 -0600

    Integration tests are fixed

--- lib/data_aggregation/index/actor_integration.rb
+++ lib/data_aggregation/index/actor_integration.rb
@@ -2,16 +2,16 @@ module DataAggregation
   module Index
     module ActorIntegration
       def start_actors
-        index_stream_name = index_category(category)
+        index_stream_name = index_category_stream_name(category)
         self::Consumers::Index.start index_stream_name
 
-        event_list_stream_name = event_list_category(category)
+        event_list_stream_name = event_list_category_stream_name(category)
         self::Consumers::EventList.start event_list_stream_name
 
-        reference_list_stream_name = reference_list_category(category)
+        reference_list_stream_name = reference_list_category_stream_name(category)
         self::Consumers::ReferenceList.start reference_list_stream_name
 
-        update_stream_name = update_category(category)
+        update_stream_name = update_category_stream_name(category)
         self::Consumers::Update.start update_stream_name
       end
     end
--- test/script/added_reference_receives_published_event.rb
+++ test/script/added_reference_receives_published_event.rb
@@ -4,7 +4,7 @@ entity_id = Identifier::UUID::Random.get
 related_entity_id = Identifier::UUID::Random.get
 related_entity_stream_name = "someRelatedEntity-#{related_entity_id}"
 
-event = Controls::SourceEvent.example
+event = Controls::SourceEvent.example entity_id: entity_id
 
 publish_event = Controls::Index::PublishEvent.build
 publish_event.(entity_id, event)
